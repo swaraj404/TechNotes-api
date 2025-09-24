73653721180f527ef514a8b938ec2f510273878e
swaraj404 <swarajpawar404@gmail.com>
2025-09-25 00:19:42 +0530

    Update CORS configuration for frontend connectivity

--- config/allowedOrigins.js
+++ config/allowedOrigins.js
@@ -1,6 +1,8 @@
 const allowedOrigins = [
-   "https://technotes-henj.onrender.com",
-
+    "https://technotes-henj.onrender.com",
+    "http://localhost:3000",
+    "http://localhost:5173",
+    "https://localhost:3000"
 ]
 
 module.exports = allowedOrigins
